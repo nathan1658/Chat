2dd6e3ffc93fcb5f04ac893011568710a2707e99
Cheng Nathan <pooikei165@gmail.com>
2018-10-26 21:43:44 +0800

    initial commit

--- Chat/FakeConversations.cs
+++ Chat/FakeConversations.cs
@@ -52,7 +52,7 @@ namespace Chat
                 msg.SubmittedDate = DateTime.Now.Add(new TimeSpan(0, -i, 0));
                 Messages.Add(msg);
             }
-            for (int i = 0; i < 100; i++)
+            for (int i = 0; i < 10000; i++)
             {
                 result.Add(new Conversation() { Title = $"Conversation {i}" });
             }
