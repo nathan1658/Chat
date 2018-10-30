33355a20c9231052c585111941c4521a8a19be35
Cheng Nathan <pooikei165@gmail.com>
2018-10-30 17:33:51 +0800

    added ChatPageRenderer

--- Chat.iOS/Renderers/ChatPageRenderer.cs
+++ Chat.iOS/Renderers/ChatPageRenderer.cs
@@ -1,10 +1,32 @@
 ﻿using System;
+using Chat.iOS.Renderers;
+using Chat.Views;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly:ExportRenderer(typeof(ChatPage),typeof(ChatPageRenderer))]
 namespace Chat.iOS.Renderers
 {
-    public class ChatPageRenderer
+    public class ChatPageRenderer:PageRenderer
     {
-        public ChatPageRenderer()
+        public override void ViewDidLoad()
         {
+            base.ViewDidLoad();
+
+
+            Xamarin.IQKeyboardManager.SharedManager.EnableAutoToolbar = false;
+            Xamarin.IQKeyboardManager.SharedManager.ShouldResignOnTouchOutside = true;
+
         }
+
+        //public override void LoadView()
+        //{
+        //    base.LoadView();
+        //    UIScrollView scrollView = new UIScrollView(UIScreen.MainScreen.Bounds);
+        //    scrollView.ContentSize = new CoreGraphics.CGSize((int)UIScreen.MainScreen.Bounds.Height, (int)UIScreen.MainScreen.Bounds.Width);
+        //    this.View = scrollView;
+        //}
+        
     }
 }
