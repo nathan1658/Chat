affbb3e8243a284986b5c0d88741b56ceba91cb1
nathan1658 <pooikei165@gmail.com>
2018-11-10 20:41:40 +0800

    Added messageBox when messageList is empty

---
+++ Chat/Converters/IsGreaterThanZeroConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Chat.Converters
+{
+    public class IsGreaterThanZeroConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return false;
+            var result = ((int)value) > 0;
+            return result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+        
+    }
+}
