f33d3905e9df5bbe84136b8393c5f6b7c5ab38c9
nathan1658 <pooikei165@gmail.com>
2018-11-10 22:22:37 +0800

    ChatPage updated button ScrollToBottom counter style, messageBoard will scale full screen

--- Chat/Views/Partials/MessageBoard.xaml.cs
+++ Chat/Views/Partials/MessageBoard.xaml.cs
@@ -18,25 +18,46 @@ namespace Chat.Views.Partials
         }
 
         void Handle_Clicked(object sender, System.EventArgs e)
-        {        
+        {
+            expandedHeight = 100;
             //Get Rendered Height..
+            var parentGrid = this.Parent as Xamarin.Forms.Grid;
 
+            if (parentGrid!=null)
+            {
+                expandedHeight = parentGrid.Height ;
+            }
             //TODO Fix ios bugs (height issue..)
             if(expandedHeight == -1)
             {
                 expandedHeight = CollapsableLayout.Height;
             }
-            expandedHeight = 100;
+            Action<bool> updateChildVisibility = new Action<bool>((x) =>
+             {
+                 if(parentGrid!=null)
+                 {
+                     foreach(var child in parentGrid.Children)
+                     {
+                         if(child!=this && child as ListView==null)
+                         {
+                             child.IsVisible = x;
+                         }
+                     }
+                 }
+             });
+           
             uint length = 500;
             if (isExpanded)
             {
                 CollapsableLayout.Animate("inv", (x) => { CollapsableLayout.HeightRequest = x; }, expandedHeight, 0, 16, length, Easing.CubicOut);
                 CollapseButton.Text = "⬇️";
-
-            }else
+                updateChildVisibility(true);
+            }
+            else
             {
                 CollapsableLayout.Animate("inv", (x) => { CollapsableLayout.HeightRequest = x; }, 0, expandedHeight, 16, length, Easing.CubicOut);
                 CollapseButton.Text = "⬆️";
+                updateChildVisibility(false);
             }
             isExpanded = !isExpanded;
 
